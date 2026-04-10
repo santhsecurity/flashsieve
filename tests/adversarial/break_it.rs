589c0cf6d3637b76cd476676f770da79cbb708b1
Mukund Thiru <contactmukundthiru@gmail.com>
2026-04-10 00:14:03 -0700

    fix FPR validation tests to assert error for invalid inputs

--- tests/adversarial/break_it.rs
+++ tests/adversarial/break_it.rs
@@ -1,5 +1,5 @@
 #![allow(clippy::cast_precision_loss, clippy::doc_markdown, clippy::pedantic)]
 #![allow(clippy::expect_used, clippy::panic, clippy::unwrap_used)]
 
-#[path = "break_it/mod.rs"]
+#[path = "../break_it/mod.rs"]
 mod break_it;
--- tests/unit/concurrent_suite.rs
+++ tests/unit/concurrent_suite.rs
@@ -1,13 +1,14 @@
 #![allow(clippy::pedantic)]
 #![allow(clippy::cast_precision_loss, clippy::doc_markdown, clippy::explicit_iter_loop, clippy::uninlined_format_args, clippy::unreadable_literal)]
 #![allow(clippy::expect_used, clippy::panic, clippy::unwrap_used)]
+#[path = "../concurrent/mod.rs"]
 mod concurrent;
 
-#[path = "concurrent/multi_writer/mod.rs"]
+#[path = "../concurrent/multi_writer/mod.rs"]
 pub mod multi_writer;
 
-#[path = "concurrent/reader_writer/mod.rs"]
+#[path = "../concurrent/reader_writer/mod.rs"]
 pub mod reader_writer;
 
-#[path = "concurrent/stress/mod.rs"]
+#[path = "../concurrent/stress/mod.rs"]
 pub mod stress;
